686ba301810a386b5dfa804d70ccf0c6761faba6
Claude J Bajada <16142659+claudebajada@users.noreply.github.com>
2025-01-28 14:30:18 +0100

    Update step_2.sh
    bug fix

--- step_2.sh
+++ step_2.sh
@@ -3,6 +3,6 @@
 mkdir GroupMRtrix3
 
 average_response ./*/MRtrix3/RF_WM.txt ./GroupMRtrix3/group_average_response_wm.txt
-average_response ./*/response_gm.txt ./GroupMRtrix3/group_average_response_gm.txt
-average_response ./*/response_csf.txt ./GroupMRtrix3/group_average_response_csf.txt
+average_response ./*/MRtrix3/RF_GM.txt ./GroupMRtrix3/group_average_response_gm.txt
+average_response ./*/MRtrix3/RF_CSF.txt ./GroupMRtrix3/group_average_response_csf.txt
 
